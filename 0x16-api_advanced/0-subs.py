cda5904d340573ef2146a3f8343c07e918898aa3
ojo-perezoso <bymartin24@gmail.com>
2022-05-31 15:59:21 +0000

    fix: documentation of number_of_subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,7 @@ import json
 
 
 def number_of_subscribers(subreddit):
+    """ Returns number of subscribers or 0 if it fails """
     if subreddit is None:
         return (0)
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
